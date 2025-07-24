79b9041685c213b1aef6bf6817ed023a3e2bc682
0xSunill <sunday7637@gmail.com>
2025-07-24 06:29:05 +0000

    program

--- anchor/programs/anchor/src/lib.rs
+++ anchor/programs/anchor/src/lib.rs
@@ -5,12 +5,36 @@ declare_id!("D4EHFhaNXjqPeyqcPkY5ftsYF4ktJYwMqmXLwpZsbti9");
 #[program]
 pub mod anchor {
     use super::*;
-
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let counter = &mut ctx.accounts.counter;
+        counter.value = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.value += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8, seeds = [b"counter", user.key().as_ref()], bump)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut, seeds = [b"counter", user.key().as_ref()], bump)]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>,
+}
+
+#[account]
+pub struct Counter {
+    pub value: u64,
+}
